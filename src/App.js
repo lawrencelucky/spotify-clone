015d0dd2abfc37bcd9b38251de7d78271178f808
lawrencelucky <lawrencelucky1999@gmail.com>
2020-12-01 12:46:02 +0100

    Feat: Adding the login auth using spotify

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import SpotifyWebApi from 'spotify-web-api-js';
 
 import './App.css';
 
+import { getTokenFromUrl } from './spotify';
+
+import Login from './Login';
+
 function App() {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = '';
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+    }
+  }, []);
+
   return (
-    <div className='app'>
-      <h1>Spotify Clone</h1>
-    </div>
+    <div className='app'>{token ? <h1>I am Logged in</h1> : <Login />}</div>
   );
 }
 
